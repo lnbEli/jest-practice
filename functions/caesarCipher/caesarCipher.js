6bffb0da974dcd9dad58325e64feba73b11b7ac6
lnbEli <137424458+lnbEli@users.noreply.github.com>
2025-06-24 15:05:22 +0200

    Preserves spaces, capitalisation  and punctuation

--- functions/caesarCipher/caesarCipher.js
+++ functions/caesarCipher/caesarCipher.js
@@ -1,1 +1,34 @@
-export default function caesarCipher() {}
+export default function caesarCipher(text, key) {
+  const textArray = text.split("");
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const alphabetArray = alphabet.split("");
+  const cipherAlphabetObj = {};
+
+  //Creates object of alphabet with corresponding cipher alaphabet
+  alphabetArray.forEach((element, index) => {
+    cipherAlphabetObj[element] = alphabetArray[(index + key) % alphabet.length];
+  });
+
+  //Creates cipher checking for uppercase
+  const cipheredTextArray = textArray.map((element) => {
+    //If space makes no change
+    if (element === " " || element === "?" || element === "!") {
+      return element;
+    } else if (element.toUpperCase() === element) {
+      return cipherAlphabetObj[element.toLowerCase()].toUpperCase();
+    } else {
+      return cipherAlphabetObj[element];
+    }
+  });
+
+  //Checks for spaces(undefined) in array and adds string space
+  const cipheredText = cipheredTextArray.map((el) => {
+    if (el === undefined) {
+      return " ";
+    } else {
+      return el;
+    }
+  });
+
+  return cipheredText.join("");
+}
--- functions/caesarCipher/tests/caesarCipher.test.js
+++ functions/caesarCipher/tests/caesarCipher.test.js
@@ -8,4 +8,44 @@ describe("caesarCipher function", () => {
   test("check is function", () => {
     expect(caesarCipher).toBeInstanceOf(Function);
   });
+
+  test("test function returns shifted phrase (1)", () => {
+    const shiftFactor = 3;
+    const input = "dog";
+    expect(caesarCipher(input, shiftFactor)).toBe("grj");
+  });
+
+  test("test function returns shifted phrase (2)", () => {
+    const shiftFactor = 20;
+    const input = "peter";
+    expect(caesarCipher(input, shiftFactor)).toBe("jynyl");
+  });
+
+  test("test function returns shifted phrase including spaces", () => {
+    const shiftFactor = 45;
+    const input = "beware the mysterious men";
+    expect(caesarCipher(input, shiftFactor)).toBe("uxptkx max frlmxkbhnl fxg");
+  });
+
+  test("test function case preservation (1)", () => {
+    const shiftFactor = 5;
+    const input = "Sweet Dude";
+    expect(caesarCipher(input, shiftFactor)).toBe("Xbjjy Izij");
+  });
+
+  test("test function case preservation (2)", () => {
+    const shiftFactor = 8;
+    const input = "I have always wanted to visit Iceland";
+    expect(caesarCipher(input, shiftFactor)).toBe(
+      "Q pidm iteiga eivbml bw dqaqb Qkmtivl"
+    );
+  });
+
+  test("test function case preservation (3)", () => {
+    const shiftFactor = 54;
+    const input = "What is happening here? You should be ashamed of yourself!";
+    expect(caesarCipher(input, shiftFactor)).toBe(
+      "Yjcv ku jcrrgpkpi jgtg? Aqw ujqwnf dg cujcogf qh aqwtugnh!"
+    );
+  });
 });
